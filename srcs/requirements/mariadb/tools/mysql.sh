8d7f7201e5d879aa5fd1adaa8661a2565b53837c
Lena MELARD <lmelard@e1r6p15.clusters.42paris.fr>
2023-03-24 18:13:18 +0100

    docker compose

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -4,17 +4,19 @@ service mysql start;
 
 sleep 3
 
+mysqladmin -u root password $SQL_ROOT_PASSWORD;
+
 mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' \
 	IDENTIFIED BY '$SQL_PASSWORD';"
 
-mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE* TO '$SQL_USER'@'%' \
+mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' \
 	IDENTIFIED BY '$SQL_PASSWORD';"
 
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "DELETE FROM mysql.user WHERE user=''"
 
-mysqladmin -u root password $SQL_ROOT_PASSWORD;
+mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
